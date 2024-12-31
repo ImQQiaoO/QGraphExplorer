bd6351c9ebe6f38e4fd2c411eb8fc41d5194d5bb
ImQQiaoO <1727344097@qq.com>
2024-12-31 23:00:32 +0800

    feat: 输入框旁添加选择式按钮

--- src/Search.cpp
+++ src/Search.cpp
@@ -1,16 +1,67 @@
 ﻿#include "Search.h"
-
+#include <QToolButton>
+#include <QMenu>
 #include "MainWindow.h"
+#include "GraphWidget.h"
+#include <QMessageBox>
+#include <spdlog/spdlog.h>
+#include "utils/Locale.hpp" 
 
 QString Search::target_string;
 
-Search::Search(GraphWidget *graphWidget) {
+Search::Search(GraphWidget *graphWidget) : QObject(graphWidget), currentCategory("聚焦") {
+    categoryList = {
+        "聚焦",
+        "测试1",
+        "测试2"
+    };
+
+    // 创建 QLineEdit
     lineEdit = new QLineEdit(graphWidget);
     lineEdit->setPlaceholderText("请输入电影或演员名...");
     lineEdit->setGeometry(20, 20, 200, 30);
     connect(lineEdit, &QLineEdit::returnPressed, this, [this, graphWidget]() { return_pressed(graphWidget); });
+
+    // 创建下拉按钮
+    dropDownButton = new QToolButton(graphWidget);
+    dropDownButton->setText("聚焦"); // 初始类别
+    dropDownButton->setPopupMode(QToolButton::MenuButtonPopup);
+    dropDownButton->setArrowType(Qt::DownArrow);
+    dropDownButton->setToolButtonStyle(Qt::ToolButtonTextOnly);
+
+    // 创建菜单并添加选项
+    QMenu *menu = new QMenu(dropDownButton);
+    for (const auto &category : categoryList) {
+        menu->addAction(category);
+    }
+    dropDownButton->setMenu(menu);
+
+    // 设置下拉按钮的位置
+    dropDownButton->setGeometry(230, 20, 80, 30);
+
+    connect(menu, &QMenu::triggered, this, &Search::dropdown_option_selected);
+    connect(dropDownButton, &QToolButton::clicked, this, &Search::button_pressed);
 }
 
+void Search::dropdown_option_selected(const QAction *action) {
+    if (action) {
+        QString selectedOption = action->text();
+        currentCategory = selectedOption; // 更新当前类别
+        dropDownButton->setText(currentCategory); // 更新按钮显示文本
+
+        std::stringstream ss;
+        ss << " 选择的类别:";
+        spdlog::info("{}", utils::utf8_to_ansi(ss.str()) + utils::utf8_to_ansi(currentCategory.toStdString()));
+    }
+}
+
+void Search::button_pressed(bool checked) {
+    currentCategory = dropDownButton->text(); // 更新当前类别
+    dropDownButton->setText(currentCategory); // 更新按钮显示文本
+    std::stringstream ss;
+    ss << " 按下时的类别:";
+    spdlog::info("{}", utils::utf8_to_ansi(ss.str()) + utils::utf8_to_ansi(currentCategory.toStdString()));
+}
 
 void Search::return_pressed(GraphWidget *graphWidget) const {
     // 获取用户输入的文本
@@ -20,6 +71,13 @@ void Search::return_pressed(GraphWidget *graphWidget) const {
     spdlog::info(utils::utf8_to_ansi(text.toStdString()));
 
     QMap<QString, VertexItem *> vertices = graphWidget->getVertices();
-    if (vertices.contains(text)) graphWidget->focus_on_vertex(text);
-    else QMessageBox::critical(nullptr, "warning", "未找到相关节点");
+    if (vertices.contains(text)) {
+        graphWidget->focus_on_vertex(text);
+    } else {
+        QMessageBox::critical(nullptr, "警告", "未找到相关节点");
+    }
+
+    std::stringstream ss;
+    ss << " 按下时的类别:";
+    spdlog::info("{}", utils::utf8_to_ansi(ss.str()) + utils::utf8_to_ansi(currentCategory.toStdString()));
 }
--- src/Search.h
+++ src/Search.h
@@ -8,13 +8,22 @@
 #include "utils/Locale.hpp"
 
 class Search : public QObject {
-    QLineEdit *lineEdit;
+    Q_OBJECT
+
 public:
     Search(GraphWidget *graphWidget);
     static QString target_string;
 
+private:
+    QLineEdit *lineEdit;
+    QToolButton *dropDownButton;
+    QString currentCategory;
+    std::vector<QString> categoryList;
+
 private slots:
     void return_pressed(GraphWidget *graphWidget) const;
+    void dropdown_option_selected(const QAction *action);
+    void button_pressed(bool checked);
 };
 
 
